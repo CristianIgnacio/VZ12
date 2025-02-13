df51eeb049debb0c83c7d8374ca7d2adc0cab6cf
CristianIgnacio <cristianignacio71@gmail.com>
2025-02-13 13:18:48 -0300

    add: funcion captura respuestas de api

--- app/components/main/bencina-service.js
+++ app/components/main/bencina-service.js
@@ -1,5 +1,54 @@
 import Tools from '../../util/tools.js';
 
+var URL = "https://apim.ea.cne.cl:8244/rendimiento_vehicular/v1/";
+
+var TOKEN = "eyJ4NXQiOiJPREJtTVRVMFpqSmpPREprTkdZMVpUaG1ZamsyWVRZek56UmpZekl6TVRCbFlqRTBNV0prWTJJeE5qZzNPRGRqWVdRNVpXWmhOV0kwTkRBM1pqTTROUSIsImtpZCI6Ik9EQm1NVFUwWmpKak9ESmtOR1kxWlRobVlqazJZVFl6TnpSall6SXpNVEJsWWpFME1XSmtZMkl4TmpnM09EZGpZV1E1WldaaE5XSTBOREEzWmpNNE5RX1JTMjU2IiwidHlwIjoiYXQrand0IiwiYWxnIjoiUlMyNTYifQ.eyJzdWIiOiIzZThjN2RmYS00M2Y5LTQ4ZGYtOGEzYi0yODNmNTU5YzE2MzIiLCJhdXQiOiJBUFBMSUNBVElPTiIsImF1ZCI6InFFbGRtRl9oeTZZdHVhWWdRV2JXeUpkblFCWWEiLCJuYmYiOjE3Mzg5MzM2OTgsImF6cCI6InFFbGRtRl9oeTZZdHVhWWdRV2JXeUpkblFCWWEiLCJzY29wZSI6ImRlZmF1bHQiLCJpc3MiOiJodHRwczovL2FwaW0uZWEuY25lLmNsOjk0NDQvb2F1dGgyL3Rva2VuIiwiZXhwIjoxNzM4OTM3Mjk4LCJpYXQiOjE3Mzg5MzM2OTgsImp0aSI6ImMwYThjYTFlLTkyY2QtNGQyMi05YzBhLWRmMzJmZmQxNzAzYiIsImNsaWVudF9pZCI6InFFbGRtRl9oeTZZdHVhWWdRV2JXeUpkblFCWWEifQ.bBgi92LyNCPSAkxmjg64ie7GWnPFJyexa_qN_0svrJteW2YyKnfDm1OYbuFW7Gq-V1kUWTImw9Dxlr8REGdBj6JK3uekdkyrj1kISWj_uEDsSKYBOE5F1fSAQ9mqOCdMYrxT5RMpEvn6rCfzaRVU8vLzGU4M4T32VwOekouUzS_jpvO3WuVocjirBtxvuVX5b0wxNAfnSzOdHRRmqk6EH663tRlZmx0Yqdss-Oj74-7gDrNXPj0Cx2woWcO99St36rMXpYY8gbCsUwcUdAtl2acEfB9JoAhUzBXwF3YEecynLnXJJx9diRmpGyb7FvWkZ1vhs_OZI98k3G3Q0oqC4Q"
+
+var LIMIT = "100";
+
+function doRequest() {
+  let totalRecords = 0;
+  let allResults = [];
+
+  // Read total records
+  return fetch(`${URL}?page=1&limit=1`, {
+      headers: {
+          'Accept': '*/*',
+          'Authorization': `Bearer ${TOKEN}`
+      }
+  })
+  .then(response => response.json())
+  .then(result => {
+      totalRecords = result.results.row.total;
+
+      // Calculate total pages
+      const totalPages = Math.ceil(totalRecords / 1000);
+      let promises = [];
+
+      /// Fetch data incrementally
+      for (let page = 1; page <= totalPages; page++) {
+          let promise = fetch(`${URL}?page=${page}&limit=${LIMIT}`, {
+              headers: {
+                  'Accept': '*/*',
+                  'Authorization': `Bearer ${TOKEN}`
+              }
+          })
+          .then(response => response.json())
+          .then(result => {
+              allResults.push(...result.results.row);
+          });
+
+          promises.push(promise);
+      }
+
+      return Promise.all(promises);
+  })
+  .then(() => allResults);
+}
+
+const result = doRequest();
+console.log(result);
+
 var parseBenci = function(newData, value) {
   if (!(value[0] in newData)) {
     newData[value[0]] = {}
